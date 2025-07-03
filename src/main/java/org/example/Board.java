8740169b6b388c5000acf4e8f6869d22fcffb2c0
Gerald Mahr <wi24b110@technikum-wien.at>
2025-07-03 18:39:28 +0200

    created classes with function layout

---
+++ src/main/java/org/example/Board.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Board {
+    private char[][] board;
+
+    public Board() {}
+
+    public boolean isCellEmpty(int x, int y) {
+        return true;
+    }
+
+    public void place(int x, int y, char marker) {}
+
+    public boolean isFull() {
+        return true;
+    }
+
+    public void clear(){}
+
+    public void print(){}
+
+}
---
+++ src/main/java/org/example/TicTacToe.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class TicTacToe {
+    private Player player1;
+    private Player player2;
+    private Player currentplayer;
+
+    public TicTacToe() {}
+
+    public void start(){}
+
+    private void switchCurrentPlayer(){}
+
+    private boolean hasWinner(){
+
+        return true;
+    }
+
+
+}
